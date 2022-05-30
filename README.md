e921010fe545af08fde41296078ec910e5335530
Sam231221 <rasengankid999@gmail.com>
2022-05-30 19:52:31 +0545

    Project Initialized

---
+++ README.md
@@ -0,0 +1,8 @@
+### Packages Used
+1. react-router-dom
+2. styled-components
+
+### Features
+1. Responsive Design
+3. CSS Components
+2. Custom Toggle hook from native hooks.
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Nav, NavLink, NavIcon, Bars } from './NavbarElements';
+
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'><span style={{color:'#ffc800'}}>Food</span>Lovers</NavLink>
+        <NavIcon onClick={toggle}>
+          <p>Menu</p>
+          <Bars />
+        </NavIcon>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
